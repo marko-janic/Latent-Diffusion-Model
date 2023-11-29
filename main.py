478b40604c99349a7e5965b37d694a955c0e7f6c
Marko Janic <marko.janic@hotmail.ch>
2023-11-29 13:05:22 +0100

    Add skeleton of code
    Still heavily work in progress

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+# Library imports
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage.transform import radon, iradon
+from omegaconf import OmegaConf
+
+# Local file imports
+from ldm.util import instantiate_from_config
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Dataset
+dataset_path = ("/mnt/c/Users/marko/Desktop/Bachelors Thesis/datasets/limited_CT/"
+                "limited-CT_64/limited-CT/horizontal_snr25.0.npz")  # local machine wsl
+
+# Autoencoder Model
+config_path = "models/vq-f4/config.yaml"
+ckpt_path = "models/vq-f4/model.ckpt"
 
 
-dataset_path = "C:/Users/marko/Desktop/Bachelors Thesis/datasets/limited_CT/" \
-               "limited-CT_64/limited-CT/horizontal_snr25.0.npz"  # on local machine
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model.cuda()
+    model.eval()
+    return model
 
 
 def main():
@@ -17,12 +42,8 @@ def main():
     x = images[0, :, :, 0]
 
     # Load pretrained autoencoder
-    autoencoder_model = torch.load("vq-f4/model.ckpt", map_location=device)
-    autoencoder_model = autoencoder_model.to(device)
-
-    x_encoded = autoencoder_model.decode_first_stage(x)
-    plt.imshow(x)
-    plt.show()
+    config = OmegaConf.load(config_path)
+    autoencoder_model = load_model_from_config(config, ckpt_path)
 
 
 if __name__ == "__main__":
