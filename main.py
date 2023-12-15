16f829667eec4a4db4484cfd66eb829c9d783bbe
marko-janic <81421890+marko-janic@users.noreply.github.com>
2023-12-15 16:47:10 +0100

    Remove unnecessary print in loss function

--- main.py
+++ main.py
@@ -134,7 +134,6 @@ def loss_fn(model, x, marginal_prob_std, autoencoder_model, eps=1e-5):
     perturbed_x = x + z * std[:, None, None, None]
 
     score = model(perturbed_x, random_t)
-    print(score.shape)
 
     loss = torch.mean(torch.sum((score * std[:, None, None, None] + z)**2, dim=(1, 2, 3)))
     return loss
